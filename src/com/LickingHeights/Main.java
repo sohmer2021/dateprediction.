2ce4ea52194a5f02c7672585d7de26647cf82aba
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-04-05 08:06:23 -0400

    int day, month, year

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,25 @@
 package com.LickingHeights;
 
-public class Main {
 
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-	
-    }
-}
+        int congruence;
+        int day;
+        int month;
+        int year;
+
+        {
+            if (month == 1)
+            {
+                month = 13;
+                year--;
+            }
+            if (month == 2)
+            {
+                month = 14;
+                year--;
+            }
+
+        }}}
