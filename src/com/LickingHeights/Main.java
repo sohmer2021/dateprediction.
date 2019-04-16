29fd30b29cb38e1d8285c3e58f24555e6808a2a9
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-04-16 07:59:50 -0400

    printed poem to screen

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,15 +5,14 @@ public class Main {
         int day;
         int month;
         int year;
+        String input;
 
         {
-            if (month == 1)
-            {
+            if (month == 1) {
                 month = 13;
                 year--;
             }
-            if (month == 2)
-            {
+            if (month == 2) {
                 month = 14;
                 year--;
             }
@@ -24,36 +23,49 @@ public class Main {
         int m;
         int j;
         int k;
-        int h = (q + 13)*(m+1)/5 + k + k/4 + j/4 + 5*j;
-        h = h%7;
-        switch (h){
+        int h = (q + 13) * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+        h = h % 7;
+        switch (h) {
 
-            {
+
                 case 0:
-                    input << "Saturday \n";
+                    input = "Saturday \n";
+                    System.out.println("Saturday's child works hard for a living");
                     break;
                 case 1:
-                    input << "Sunday \n";
+                    input = "Sunday \n";
+                    System.out.println("Sunday's child is fair and wise and good in every way");
                     break;
                 case 2:
-                    input << "Monday \n";
+                    input = "Monday \n";
+                    System.out.println("Monday's child is fair of face");
                     break;
                 case 3:
-                    input << "Tuesday \n";
+                    input = "Tuesday \n";
+                    System.out.println("Tuesday's child is full of grace");
                     break;
                 case 4:
-                    input << "Wednesday \n";
+                    input = "Wednesday \n";
+                    System.out.println("Wednesday's child is full of woe");
                     break;
                 case 5:
-                    input << "Thursday \n";
+                    input = "Thursday \n";
+                    System.out.println("Thursday's child has far to go");
                     break;
                 case 6:
-                    input << "Friday \n";
+                    input = "Friday \n";
+                    System.out.println("Friday's child is loving and giving");
                     break;
             }
+        /*System.out.println("Monday's child is fair of face");
+        System.out.println("Tuesday's child is full of grace");
+        System.out.println("Wednesday's child is full of woe");
+        System.out.println("Thursday's child has far to go");
+        System.out.println("Friday's child is loving and giving");
+        System.out.println("Saturday's child works hard for a living");
+        System.out.println("Sunday's child is fair and wise and good in every way");*/
 
+        }
 
+    }
 
-
-
-    }}}
